c59a8988c82de3c5f025b8d7d2f32fefb01af61e
bojanicmilos <75372710+bojanicmilos@users.noreply.github.com>
2021-04-05 14:35:04 +0200

    Added users repository and domain folders.

---
+++ Pozoriste.Repository/UsersRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pozoriste.Data.Context;
+using Pozoriste.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pozoriste.Repository
+{
+    public interface IUsersRepository : IRepository<User>
+    {
+
+    }
+    public class UsersRepository : IUsersRepository
+    {
+        private TheatreContext _theatreContext;
+
+        public UsersRepository(TheatreContext theatreContext)
+        {
+            _theatreContext = theatreContext;
+        }
+
+        public async Task<User> Delete(int id)
+        {
+            User existing = await _theatreContext.Users.FindAsync(id);
+            var result = _theatreContext.Users.Remove(existing);
+
+            return result.Entity;
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            var data = await _theatreContext.Users.ToListAsync();
+
+            return data;
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            var data = await _theatreContext.Users.FindAsync(id);
+
+            return data;
+        }
+
+        public User Insert(User obj)
+        {
+            return _theatreContext.Users.Add(obj).Entity;
+        }
+
+        public void Save()
+        {
+            _theatreContext.SaveChanges();
+        }
+
+        public User Update(User obj)
+        {
+            _theatreContext.Entry(obj).State = EntityState.Modified;
+
+            return obj;
+        }
+    }
+}
