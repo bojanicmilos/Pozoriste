0f79e280371f3d9af71b79d256bd6c81107819bc
Koloper2000 <koloperdavid@gmail.com>
2021-04-06 21:33:39 +0200

    Added address service and controller

---
+++ Pozoriste.API/Controllers/AddressesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pozoriste.Domain.Common;
+using Pozoriste.Domain.Interfaces;
+using Pozoriste.Domain.Models;
+using Pozoriste.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pozoriste.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly IAddressService _addressService;
+
+        public AddressesController(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AddressDomainModel>> GetAddressById(int id)
+        {
+            var address = await _addressService.GetByIdAsync(id); 
+
+            if (address == null)
+            {
+                return NotFound(Messages.ADDRESS_NOT_FOUND);
+            }
+            return Ok(address);
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<ActionResult<IEnumerable<AddressDomainModel>>> GetAllAddresses()
+        {
+            IEnumerable<AddressDomainModel> addressDomainModels;
+            addressDomainModels = await _addressService.GetAllAsync();
+
+            if (addressDomainModels.Count() == 0)
+            {
+                addressDomainModels = new List<AddressDomainModel>();
+
+            }
+
+            return Ok(addressDomainModels);
+        } 
+
+    }
+}
---
+++ Pozoriste.Domain/Services/AddressService.cs
@@ -0,0 +1,71 @@
+﻿using Pozoriste.Data.Entities;
+using Pozoriste.Domain.Interfaces;
+using Pozoriste.Domain.Models;
+using Pozoriste.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pozoriste.Domain.Services
+{
+    public class AddressService : IAddressService
+    {
+        private readonly IAddressesRepository _addressesRepository;
+
+        public AddressService(IAddressesRepository addressesRepository)
+        {
+            _addressesRepository = addressesRepository;
+        }
+       
+       /* public async Task<AddressDomainModel> AddAddress(AddressDomainModel addressModel)
+        {
+            
+        }
+*/
+        public async Task<IEnumerable<AddressDomainModel>> GetAllAsync()
+        {
+            var addresses = await _addressesRepository.GetAllAsync();
+
+            if (addresses.Count() == 0)
+            {
+                return null;
+            }
+
+            List<AddressDomainModel> addressDomainModels = new List<AddressDomainModel>();
+
+            foreach (var address in addresses)
+            {
+                AddressDomainModel addressDomainModel = new AddressDomainModel
+                {
+                    Id = address.Id,
+                    CityName = address.CityName,
+                    StreetName = address.StreetName
+
+                };
+                addressDomainModels.Add(addressDomainModel);
+            }
+
+            return addressDomainModels;
+        }
+
+        public async Task<AddressDomainModel> GetByIdAsync(int id)
+        {
+            var address = await _addressesRepository.GetByIdAsync(id);
+            
+            if (address == null)
+            {
+                return null;
+            }
+
+            AddressDomainModel addressDomainModel = new AddressDomainModel
+            {
+                Id = address.Id,
+                CityName = address.CityName,
+                StreetName = address.StreetName
+            };
+            return addressDomainModel;
+        }
+    }
+}
