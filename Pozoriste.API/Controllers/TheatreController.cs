8372cedcdcec243a2f58216c0baa3c5b9da88bda
Pavlovic023 <47660922+Lucifer023@users.noreply.github.com>
2021-04-07 19:00:34 +0200

    Added controller for getting all theatres

---
+++ Pozoriste.API/Controllers/TheatreController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pozoriste.Domain.Interfaces;
+using Pozoriste.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pozoriste.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TheatreController : ControllerBase
+    {
+        private readonly ITheatreService _theatreService;
+        private readonly IAddressService _addressService;
+
+        public TheatreController(ITheatreService theatreService, IAddressService addressService)
+        {
+            _theatreService = theatreService;
+            _addressService = addressService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TheatreDomainModel>>> GetAsync()
+        {
+            IEnumerable<TheatreDomainModel> theatreDomainModels;
+
+            theatreDomainModels = await _theatreService.GetAllAsync();
+
+            if(theatreDomainModels == null)
+            {
+                theatreDomainModels = new List<TheatreDomainModel>();
+            }
+
+            return Ok(theatreDomainModels);
+        }
+    }
+}
