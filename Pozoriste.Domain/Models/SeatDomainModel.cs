2ca07ab527ace68f4fbcf3f93d1f24566f929550
Pavlovic023 <47660922+Lucifer023@users.noreply.github.com>
2021-04-07 18:27:48 +0200

    Added repository, service(create and delete methods for Theatre), AuditoriumDomainModel, SeatDomainModel, TheatreDomainModel and interface for TheatreService

---
+++ Pozoriste.Domain/Models/SeatDomainModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pozoriste.Domain.Models
+{
+    public class SeatDomainModel
+    {
+        public int Id { get; set; }
+        public int AuditoriumId { get; set; }
+        public int Row { get; set; }
+        public int Number { get; set; }
+
+    }
+}
