85067fe0f7965c1e38e89439702cf682a5309d7b
Koloper2000 <koloperdavid@gmail.com>
2021-04-17 23:59:57 +0200

    Side menu design

--- Pozoriste.FrontendApp/my-app/src/components/SideMenu.js
+++ Pozoriste.FrontendApp/my-app/src/components/SideMenu.js
@@ -10,31 +10,42 @@ const SideMenu = () => {
     return (
         <div className='side-menu'>
             <Link className='side-menu-element' to="/showlist">Predstave</Link>
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/piecealllist">Pozorisni komadi</Link>
+                
             }
+            <span class="divider"></span>
             <Link className='side-menu-element' to="/pieceactivelist">Aktivni pozorisni komadi</Link>
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/addPiece">Dodaj komad</Link>
             }
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/addactor">Dodaj glumca</Link>
             }
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/actorlist">Svi glumci</Link>
             }
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/addshow">Dodaj predstavu</Link>
             }
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/addtheatre">Dodaj pozoriste</Link>
             }
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/showalltheatres">Pozorista</Link>
             }
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/showallAuditoriums">Sale</Link>
             }
+            <span class="divider"></span>
             {
                 (isUserLogged() && getRole() === 'admin') && <Link className='side-menu-element' to="/addAuditorium">Dodaj salu</Link>
             }
